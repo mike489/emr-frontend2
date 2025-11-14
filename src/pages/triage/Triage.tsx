78aa82a8806e3f7f641363e28cd86eed7a1c49b1
Birku Woldie <birkuwo88@gmail.com>
2025-11-14 12:35:37 +0300

    feat: creating appt, update the tabs, applayout, update the whole code

--- src/pages/triage/Triage.tsx
+++ src/pages/triage/Triage.tsx
@@ -21,7 +21,6 @@ import {
 } from '@mui/material';
 import { Send, Eye, FileUp, FileSearch } from 'lucide-react';
 import { Search, ArrowDropDown, ArrowBackIos } from '@mui/icons-material';
-import AppLayout from '../../layouts/AppLayout';
 import { useNavigate } from 'react-router-dom';
 import { PatientService } from '../../shared/api/services/patient.service';
 import { toast } from 'react-toastify';
